51b57b215567851e1029182ecb2bce23a6b55d9c
Simon Gheux <simongheux@MacBook-Pro-de-Simon.local>
2018-04-19 14:20:06 +0200

    Boutons menu dans un tableau avec le système de router

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,11 @@
 <template>
- <!-- <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div> -->
   <div id="app">
     <header class="text-center container">
       <h1 class="my-5">{{ titre }}</h1>
       <ul class="row text-center mx-auto list-unstyled">
-        <li class="col"><router-link class="btn btn-danger"  to="/exo1" @mouseover="changeTitle">Exo1
+        <li class="col" @mouseover="changeTitle" @mouseout="titleBase" v-for="(bouton,index) in boutons" :key="index"><router-link class="btn btn-danger"  :to="bouton.valeur" >{{ bouton.name }}
         </router-link></li>
-        <li class="col"><router-link class="btn btn-danger"  to="/exo2" @mouseover="changeTitle">Exo2
-        </router-link></li>
-        <li class="col"><router-link class="btn btn-danger"  to="/exo3" @mouseover="changeTitle">Exo3
-        </router-link></li>
-        <li class="col"><router-link class="btn btn-danger"  to="/exo4" @mouseover="changeTitle">Exo4
-        </router-link></li>
-        <li class="col"><router-link class="btn btn-danger"  to="/exo5" @mouseover="changeTitle">Exo5
-        </router-link></li>
-        <li class="col"><router-link class="btn btn-danger"  to="/exo6" @mouseover="changeTitle">Exo6
-        </router-link></li>
-        <li class="col"><router-link class="btn btn-danger"  to="/exo7" @mouseover="changeTitle">Exo7
-        </router-link></li>
-        
-        
-
+    
       </ul>
     </header>
 
@@ -47,12 +26,48 @@ export default {
   mounted() {},
   data() {
     return {
-      titre: "DOM Javascript - VueJS"
+      titre: "DOM Javascript - VueJS",
+      titleChanged: false,
+      boutons: [
+        {
+          name: "Exo 1",
+          valeur: "exo1"
+        },
+        {
+          name: "Exo 2",
+          valeur: "exo2"
+        },
+        {
+          name: "Exo 3",
+          valeur: "exo3"
+        },
+        {
+          name: "Exo 4",
+          valeur: "exo4"
+        },
+        {
+          name: "Exo 5",
+          valeur: "exo5"
+        },
+        {
+          name: "Exo 6",
+          valeur: "exo6"
+        },
+        {
+          name: "Exo 7",
+          valeur: "exo7"
+        }
+      ]
     };
   },
   methods: {
     changeTitle() {
       this.titre = event.target.textContent;
+      this.titleChanged = true;
+    },
+    titleBase() {
+      this.titre = "DOM Javascript - VueJS";
+      this.titleChanged = false;
     }
   }
 };
